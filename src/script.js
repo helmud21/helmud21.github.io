717a1b81b045f2a450607801cdf301bbe2b978b2
helmud panggabean <38092137+helmud21@users.noreply.github.com>
2023-07-04 22:47:17 +0700

    menambahkan konten profil dan kontak

---
+++ src/script.js
@@ -0,0 +1,23 @@
+var images = document.getElementsByTagName("img");
+var contactValue = document.getElementById("contactValue");
+
+const buttonPressed = e => { 
+    var id = e.target.id;
+    if (id == 1) {
+        contactValue.innerHTML = `<h1>0852-6772-7717</h1>`;
+    } else if (id == 2) {
+        contactValue.innerHTML = `<h1>helmudpgbn@gmail.com</h1>`;
+    } else if (id == 3) {
+        contactValue.innerHTML = `<h1>Maaf saya belum memiliki akun pada media sosial ini</h1>`;
+    } else if (id == 4) {
+        contactValue.innerHTML = `<h1>helmudpgbn / Helmud Panggabean</h1>`;
+    } else if (id == 5) {
+        contactValue.innerHTML = `<h1>Helmud Panggabean</h1>`;
+    } else {
+        contactValue.innerHTML = `<h1>ID Tidak Ditemukan!!!</h1>`;
+    }
+  }
+
+  for (let image of images) {
+    image.addEventListener("click", buttonPressed);
+  }
